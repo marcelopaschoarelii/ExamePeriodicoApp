662521a5b58570a9e36a04e7dfb026d72c20354a
Marcelo Paschoareli De Moura Barros <marcelobarros-ieg@instituto.ad>
2025-06-10 12:09:30 -0300

    Recycle View

---
+++ app/src/main/java/com/example/exameperiodicojf/adapter/HistoricoAdapter.java
@@ -0,0 +1,14 @@
+package com.example.exameperiodicojf.adapter;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class HistoricoAdapter extends RecyclerView.ViewHolder {
+    public HistoricoAdapter(View itemView) {
+        super(itemView);
+    }
+
+
+}
